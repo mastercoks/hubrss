d6003dd640002d920adee36ed4fb80ac5a18e378
mastercoks <matheusbdo1@gmail.com>
2019-11-01 04:07:09 -0300

    Ajustando path

--- template.js
+++ template.js
@@ -14,14 +14,14 @@ const newsCategoryTemplate = (news) => {
   html += `<div class="col-md-12 row">`;
   html +=   news.image ? `<div class="col-md-4"><img src="${news.image}" alt="Imagem"></div><div class="card-body col-md-8">` : `<div class="card-body">`;
   html +=  `<strong class="d-inline-block mb-2 ${news.class}">${news.author.name}</strong>
-            <a href="noticia/?id=${news.id}" class="stretched-link ${news.class}"><h3 class="mb-0">${news.title}</h3></a>
+            <a href="../noticia/?id=${news.id}" class="stretched-link ${news.class}"><h3 class="mb-0">${news.title}</h3></a>
           </div></div>`;
   return html;
 }
 
 const principalNewsTemplate = (news) => {
   return `
-      <div class="card jumbotron p-4 p-md-5" style="background-image: url(${news.image})">
+      <div class="card jumbotron p-4 p-md-5" style="height:700px; background-image: url(${news.image})">
         <div class="card-body">
         <div class="card-body">
           <a href="noticia/?id=${news.id}" class="stretched-link"><h1 class="text-white display-4 font-italic">${news.title}</h1></a>
